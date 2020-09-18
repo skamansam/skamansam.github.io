74da34ffc39cdf7c946e573aebe8b6b478a8e5e4
Samuel C. Tyler <skamansam@gmail.com>
2020-09-18 13:53:14 +0000

    update deploy.sh so we can deploy

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,25 @@
 # abort on errors
 set -e
 
+# clear out deploy directory
+rm -rf docs/*
+
 # build
 yarn build
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-cp CNAME dist/CNAME
+# cp CNAME docs/CNAME
 
 # navigate into the build output directory
-cd dist
+cd docs
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:skamansam/skamansam.github.io.git master
+git push -f https://github.com/skamansam/skamansam.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
