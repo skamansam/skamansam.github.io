a1e5b2e4bc85fba6a415fb1f6146aa635f398e82
Samuel C. Tyler <skamansam@gmail.com>
2020-12-15 04:45:09 +0000

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # build
-npm run docs:build
+yarn build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
+cd docs/
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f https://github.com/skamansam/skamansam.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
