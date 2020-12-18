935e0a2092828a672a3f2bb1841accc48f27898f
Samuel C Tyler <skamansam@gmail.com>
2020-12-18 01:49:54 -0500

    init vuepress

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,43 +1,71 @@
-// See more in https://github.com/ktquez/vuepress-theme-ktquez#configuration
-const path = require('path')
-const head = require('./config/head')
-const themeConfig = require('./config/themeConfig')
-const resolve = pathName => path.join(__dirname, pathName)
+const { description } = require('../../package')
+
 module.exports = {
-  // theme: 'ktquez',
-  head,
-  themeConfig,
-  base: '/',
-  title: `Rude Boy Enterprises`,  
-  // ga: `MY TRACKING CODE GOOGLE ANALYTICS`,
-  evergreen: true,  // For modern browsers
-  serviceWorker: true,
-  markdown: {
-    // options for markdown-it-anchor
-    // anchor: { permalink: false },
-    extendMarkdown: md => {
-      md.use(require('markdown-it-katex'));
-    }
-  },
-  locales: {
-    '/': {
-      lang: 'en'
-    },
-    // '/pt/': {
-    //   lang: 'pt-br'
-    // },
-    // '/es/': {
-    //   lang: 'es'
-    // }
-  },
-  extraWatchFiles: ['**/*.md', '**/*.vue'],
-  configureWebpack () {
-    return {
-      resolve: {
-        alias: {
-          '@public': resolve('./public'),
-        }
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Vuepress Docs Boilerplate',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'Config',
+        link: '/config/'
+      },
+      {
+        text: 'VuePress',
+        link: 'https://v1.vuepress.vuejs.org'
       }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
     }
-  }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+    '@vuepress/blog',
+  ]
 }
--- src/posts/README.md
+++
@@ -1,7 +0,0 @@
----
-view: posts
-title: All posts of Skaman Sam Tyler
-excerpt: Articles!
-lang: en
-author: skamansam
----
