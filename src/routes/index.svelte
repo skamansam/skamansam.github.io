8e06db3a4761d50677a27067110b7fd59c934301
Samuel C. Tyler <skamansam@gmail.com>
2022-06-19 00:03:29 -0400

    add sections for latest posts and projects

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
+  import Card from '../components/Card.svelte'
 	let clazz = '';
 	export { clazz as class };
 </script>
 
 <svelete:head><title>Welcome to Rude Boy Solutions!</title></svelete:head>
 <div class={clazz}>
-	<h1>Welcome to Rude Boy Solutions!</h1>
-	<p>This site is the online home of me, Samuel "Skaman Sam" Tyler. It outlines my adventures 
-  in learning; its many problems and its many solutions. I hope you take this adventure with 
-  me, at least one small one, and I hope it benefits you greatly... or only a little, as the case may be.</p>
+  <Card title="Welcome to Rude Boy Solutions!">
+    <p>This site is the online home of me, Samuel "Skaman Sam" Tyler. It outlines my adventures 
+    in learning; its many problems and its many solutions. I hope you take this adventure with 
+    me, at least one small one, and I hope it benefits you greatly... or only a little, as the case may be.</p>
+  </Card>
+  <h2>Latest Posts</h2>
+
+  <h2>Latest Projects</h2>
 </div>
-  
+
