7669d7e4156dd447f55484758711c8972c5ba06f
Samuel C. Tyler <skamansam@gmail.com>
2022-06-19 00:01:48 -0400

    add better html support to posts

--- src/layouts/post.svelte
+++ src/layouts/post.svelte
@@ -1,71 +1,71 @@
 <script>
-  import { page } from '$app/stores';
-  import { onMount } from 'svelte';
-	
-  export let title = '';
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+
+	export let title = '';
 	export let author = '';
 	export let lastmod = '';
 	export let description = '';
-  export let meta = [];  
+	export let meta = [];
 	export let updated_at = '';
 	export let created_at = '';
 	export let tags = [];
 	export let categories = [];
 	export let coverAlt = '';
 	export let coverImage = '';
-  export let excerpt = '';
-  export let comments = '';
-  export let view = '';
-  export let layout = '';
-  export let lang = '';
+	export let excerpt = '';
+	export let comments = '';
+	export let view = '';
+	export let layout = '';
+	export let lang = '';
 
-  /** @type string|null */
-  export let cover = null;
-  /** @type string|null */
-  export let opengraphImage = null;
-  /** @type string|null */
-  export let twitterImage = null;
+	/** @type string|null */
+	export let cover = null;
+	/** @type string|null */
+	export let opengraphImage = null;
+	/** @type string|null */
+	export let twitterImage = null;
 
-  if (!twitterImage) twitterImage = opengraphImage || cover;
-  if (!opengraphImage) opengraphImage = twitterImage || cover;
+	if (!twitterImage) twitterImage = opengraphImage || cover;
+	if (!opengraphImage) opengraphImage = twitterImage || cover;
 
-  let pageUrl = '';
-  let siteUrl = '';  
-  let socialCardType = (opengraphImage || twitterImage || cover) ? 'summary_large_image' : 'summary';
+	let pageUrl = '';
+	let siteUrl = '';
+	let socialCardType = opengraphImage || twitterImage || cover ? 'summary_large_image' : 'summary';
 
-  onMount(() => {
-    siteUrl = window.location.href;
-    pageUrl = siteUrl + page.path;
-  });
-  
-  /** @type {remarkHeadingPlugin[]} */
+	onMount(() => {
+		siteUrl = window.location.href;
+		pageUrl = siteUrl + page.path;
+	});
+
+	/** @type {remarkHeadingPlugin[]} */
 	export let headings = [];
 </script>
 
 <svelte:head>
-  <!-- TODO: add OpenGraph stuff here to fill in more metadata for each og:* entry. https://css-tricks.com/essential-meta-tags-social-media/ -->
-  {#each meta as prop}
-    <meta property="{prop.property}" content="{prop.content}" />
-  {/each}
-  
-  <!-- Facebook Meta Tags -->
-  <meta property="og:site_name" content="Rude Boy Solutions" />
-  <meta property="article:published_time" content="{updated_at}" />
-  <meta property="article:author" content="{author}" />
-  <meta property="og:url" content="{pageUrl}" />
-  <meta property="og:type" content="article"/>
-  <meta property="og:title" content="{title} - RBE" />
-  <meta property="og:image" content="{opengraphImage || twitterImage || cover}" />
-  <meta property="og:description" content="{description}"/>
+	<!-- TODO: add OpenGraph stuff here to fill in more metadata for each og:* entry. https://css-tricks.com/essential-meta-tags-social-media/ -->
+	{#each meta as prop}
+		<meta property={prop.property} content={prop.content} />
+	{/each}
 
-  <!-- Twitter Meta Tags (twitter also uses og tags) -->
-  <meta name="twitter:card" content="{socialCardType}"/>
-  <meta property="twitter:site" content="@skamansam"/>
-  <meta name="twitter:creator" content="@{author}"/>
-  <meta name="twitter:image" content="{twitterImage || opengraphImage || cover}"/>
+	<!-- Facebook Meta Tags -->
+	<meta property="og:site_name" content="Rude Boy Solutions" />
+	<meta property="article:published_time" content={updated_at} />
+	<meta property="article:author" content={author} />
+	<meta property="og:url" content={pageUrl} />
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content="{title} - RBE" />
+	<meta property="og:image" content={opengraphImage || twitterImage || cover} />
+	<meta property="og:description" content={description} />
 
-  <title>{title} | RBE</title>
-  <meta name="description" content="{description}"/>
+	<!-- Twitter Meta Tags (twitter also uses og tags) -->
+	<meta name="twitter:card" content={socialCardType} />
+	<meta property="twitter:site" content="@skamansam" />
+	<meta name="twitter:creator" content="@{author}" />
+	<meta name="twitter:image" content={twitterImage || opengraphImage || cover} />
+
+	<title>{title} | RBE</title>
+	<meta name="description" content={description} />
 </svelte:head>
 
 <!-- <meta property="og:site_name" content="Rude Boy Solutions" />
@@ -87,13 +87,16 @@
 </div> -->
 
 <main>
-  <h1>{title}</h1>
-  <p class="date created_at">posted on: {new Date(created_at || null).toLocaleString()}</p>
-  {#if updated_at !== created_at}
-    <p class="date updated_at">last updated: {new Date(updated_at || null).toLocaleString()}</p>
-  {/if}
-  <p class="author">by: {author}</p>
-  <div class="description hidden">{description}</div>
-  <slot>
-  </slot>
+	<article>
+		<hgroup>
+			<h1>{title}</h1>
+			<p class="date created_at">posted on: {new Date(created_at || null).toLocaleString()}</p>
+			{#if updated_at !== created_at}
+				<p class="date updated_at">last updated: {new Date(updated_at || null).toLocaleString()}</p>
+			{/if}
+			<p class="author">by: {author}</p>
+		</hgroup>
+		<div class="description hidden">{description}</div>
+		<slot />
+	</article>
 </main>
