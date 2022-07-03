683256bf8d183736f33ce5f514b69dbe6aa31c0c
Samuel C. Tyler <skamansam@gmail.com>
2022-07-03 01:32:49 -0400

    add sitemap to footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -7,6 +7,8 @@
 <div class="{clazz} footer flex w-full border-t-2 py-2">
 	<div class="self-center ml-1">
 		Made with 💖 by "Skaman Sam" Tyler<br />
+    <!-- add here so we have a link to the sitemap -->
+    <a href="/sitemap.xml">Sitemap</a>
 	</div>
   <BuyMeACoffee scale="0.9" class="m-0 ml-auto mr-1"/>
 </div>
