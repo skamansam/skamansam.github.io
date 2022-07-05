5e83f9abeb0121edb191bb39396850489a920074
skamansam <skamansam@users.noreply.github.com>
2022-07-05 05:44:53 +0000

    deploy: 7b94700acc625f5300800b3d1d9c5a56ff9d4500

---
+++ _app/immutable/pages/posts/2009-05-10-tetex-for-puppy-linux.md-1888d69a.js
@@ -0,0 +1,41 @@
+import{S as V,i as X,s as ee,D as H,x as te,y as oe,z as ae,A as ne,B as M,r as ie,p as se,C as re,L as N,e as c,t as l,k as P,c as w,a as v,h as d,d as o,m as S,b as C,g as i,I as s,n as le}from"../../chunks/index-a5078a7d.js";import{P as de}from"../../chunks/post-027d9162.js";function pe(x){let t,p,f,u,a,r,m,B,T,I,j,L,g,R,E,h,A,y,U,z,_,D,F,$,b,G;return{c(){t=c("p"),p=l("SORRY!: The current sfs file is broken. I am working as fast as I can to fix this, so check back for more!"),f=P(),u=c("p"),a=l(`I have recently had to send my new laptop back for a power board replacement, so
+I have resurrected my old laptop. It is a HP Pavilion ze4900 that my coworker
+gave me 2 years ago. (It was an old one he didn\u2019t want.) This laptop was pretty
+old when I got it, and a series of unfortunate events occured.`),r=P(),m=c("p"),B=l(`First, the power cord got fragged. Then the battery died. Lastly, the hard drive
+failed. With all these problems, there was only one solution: a small,
+lightweight linux distro to use as a dumb terminal for my server. So I installed
+Puppy Linux on a CD, ran it with a USB drive and worked on my Summer of Code
+project with it. I loved Puppy Linux!`),T=P(),I=c("p"),j=l(`Since I am now in that same position, I have decided to update my version of
+Puppy from 4.00 to 4.20. I also found out that without a hard drive even
+attached, this laptop will boot from a USB drive. Since I now have a 8 Gb drive
+and plenty of space, I decided to install a few extra programs, including the
+development .sfs file, which allows me to build software for this excellent
+distro!`),L=P(),g=c("p"),R=l(`I really wanted tetex and a good editor, but don\u2019t really like installing
+pups/pets. I prefer sfs files, so I can have better control over what is
+installed and how it is installed. So I got down to it!`),E=P(),h=c("p"),A=l(`I went over to the the Puppy Linux forum to find out how to make sfs files, and
+found a great tool `),y=c("a"),U=l("here"),z=l(`. Then I
+downloaded tetex and texmf, compiled it in /usr/local/tetex, and created `),_=c("a"),D=l(`this sfs
+file`),F=l(`. To use it, just put it in the same location as your pup_save.2fs file
+and reboot. If you want to use it with other versions of Puppy Linux, change the
+420 in the filename to 400 or 300 or whatever the standard is for your version.`),$=P(),b=c("p"),G=l(`I am also working on incorporating several tex editors with it, so keep checking
+back for more!`),this.h()},l(e){t=w(e,"P",{});var n=v(t);p=d(n,"SORRY!: The current sfs file is broken. I am working as fast as I can to fix this, so check back for more!"),n.forEach(o),f=S(e),u=w(e,"P",{});var O=v(u);a=d(O,`I have recently had to send my new laptop back for a power board replacement, so
+I have resurrected my old laptop. It is a HP Pavilion ze4900 that my coworker
+gave me 2 years ago. (It was an old one he didn\u2019t want.) This laptop was pretty
+old when I got it, and a series of unfortunate events occured.`),O.forEach(o),r=S(e),m=w(e,"P",{});var W=v(m);B=d(W,`First, the power cord got fragged. Then the battery died. Lastly, the hard drive
+failed. With all these problems, there was only one solution: a small,
+lightweight linux distro to use as a dumb terminal for my server. So I installed
+Puppy Linux on a CD, ran it with a USB drive and worked on my Summer of Code
+project with it. I loved Puppy Linux!`),W.forEach(o),T=S(e),I=w(e,"P",{});var Y=v(I);j=d(Y,`Since I am now in that same position, I have decided to update my version of
+Puppy from 4.00 to 4.20. I also found out that without a hard drive even
+attached, this laptop will boot from a USB drive. Since I now have a 8 Gb drive
+and plenty of space, I decided to install a few extra programs, including the
+development .sfs file, which allows me to build software for this excellent
+distro!`),Y.forEach(o),L=S(e),g=w(e,"P",{});var Z=v(g);R=d(Z,`I really wanted tetex and a good editor, but don\u2019t really like installing
+pups/pets. I prefer sfs files, so I can have better control over what is
+installed and how it is installed. So I got down to it!`),Z.forEach(o),E=S(e),h=w(e,"P",{});var k=v(h);A=d(k,`I went over to the the Puppy Linux forum to find out how to make sfs files, and
+found a great tool `),y=w(k,"A",{title:!0,href:!0});var q=v(y);U=d(q,"here"),q.forEach(o),z=d(k,`. Then I
+downloaded tetex and texmf, compiled it in /usr/local/tetex, and created `),_=w(k,"A",{title:!0,href:!0});var J=v(_);D=d(J,`this sfs
+file`),J.forEach(o),F=d(k,`. To use it, just put it in the same location as your pup_save.2fs file
+and reboot. If you want to use it with other versions of Puppy Linux, change the
+420 in the filename to 400 or 300 or whatever the standard is for your version.`),k.forEach(o),$=S(e),b=w(e,"P",{});var K=v(b);G=d(K,`I am also working on incorporating several tex editors with it, so keep checking
+back for more!`),K.forEach(o),this.h()},h(){C(y,"title","Edit-SFS"),C(y,"href","http://www.murga-linux.com/puppy/viewtopic.php?t=12112"),C(_,"title","tetex/texmf file"),C(_,"href","http://rbe.homeip.net/content/downloads/files/texmf_420.sfs")},m(e,n){i(e,t,n),s(t,p),i(e,f,n),i(e,u,n),s(u,a),i(e,r,n),i(e,m,n),s(m,B),i(e,T,n),i(e,I,n),s(I,j),i(e,L,n),i(e,g,n),s(g,R),i(e,E,n),i(e,h,n),s(h,A),s(h,y),s(y,U),s(h,z),s(h,_),s(_,D),s(h,F),i(e,$,n),i(e,b,n),s(b,G)},p:le,d(e){e&&o(t),e&&o(f),e&&o(u),e&&o(r),e&&o(m),e&&o(T),e&&o(I),e&&o(L),e&&o(g),e&&o(E),e&&o(h),e&&o($),e&&o(b)}}}function fe(x){let t,p;const f=[x[0],Q];let u={$$slots:{default:[pe]},$$scope:{ctx:x}};for(let a=0;a<f.length;a+=1)u=H(u,f[a]);return t=new de({props:u}),{c(){te(t.$$.fragment)},l(a){oe(t.$$.fragment,a)},m(a,r){ae(t,a,r),p=!0},p(a,[r]){const m=r&1?ne(f,[r&1&&M(a[0]),r&0&&M(Q)]):{};r&2&&(m.$$scope={dirty:r,ctx:a}),t.$set(m)},i(a){p||(ie(t.$$.fragment,a),p=!0)},o(a){se(t.$$.fragment,a),p=!1},d(a){re(t,a)}}}const Q={comments:!0,view:"post",layout:"post",lang:"en",author:"skamansam",title:"TeTex for Puppy Linux",description:"Creating a TeTex .pup for Puppy Linux 4.20",excerpt:"I really wanted tetex and a good editor, but don't really like installing pups/pets",cover:!1,coverImage:!1,coverAlt:"RBE Logo",categories:["linux"],tags:["Puppy Linux"],created_at:"2009-05-10T17:02:00.000Z",updated_at:"2009-05-10T17:02:00.000Z"};function ue(x,t,p){return x.$$set=f=>{p(0,t=H(H({},t),N(f)))},t=N(t),[t]}class ce extends V{constructor(t){super(),X(this,t,ue,fe,ee,{})}}export{ce as default,Q as metadata};
