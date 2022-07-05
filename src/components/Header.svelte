4729e909bf162a10d638c7c31b6818baf188a9b0
Samuel C. Tyler <skamansam@gmail.com>
2022-07-05 01:37:40 -0400

    use icon  and dropdown instead of complicated view for theme selection

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+	import { Moon, Sun } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
 	import { createEventDispatcher } from 'svelte';
-  import RBELogo from './RBELogo.svelte';
+	import RBELogo from './RBELogo.svelte';
 	let clazz = '';
 	export { clazz as class };
 	export let theme = '';
@@ -9,49 +11,32 @@
 	let isDark = brightness === 'dark';
 
 	const dispatch = createEventDispatcher();
+	const updateBrightness = () => (isDark = !isDark);
 
 	$: dispatch('brightnessChange', isDark ? 'dark' : 'light');
 	$: dispatch('themeChange', theme);
 </script>
 
 <header class={`site-head flex ${clazz || ''}`}>
-  <div class="header-logo inline-block text-onPrimaryBg">
-    <RBELogo alt="Rude Boy Enterprises Logo" width="100" height="100"/>
-        <!-- <img src="/rbe-logo.svg" alt="Rude Boy Enterprises Logo" width="100" height="100"> -->
-  </div>
+	<div class="header-logo inline-block text-onPrimaryBg">
+		<RBELogo width="100" height="100" />
+		<!-- <img src="/rbe-logo.svg" alt="Rude Boy Enterprises Logo" width="100" height="100"> -->
+	</div>
 	<div class="flex flex-col grow justify-center ml-3">
 		<div class="text-4xl text-onPrimaryBg">Rude Boy Solutions</div>
 		<div class=" indent-5">Solutions. For Life.</div>
 	</div>
 	<div class="mt-2 mr-2 text-sm">
-		<label for="themeSelector">Theme: </label>
-		<select bind:value={theme} id="themeSelector">
+		<span on:click={updateBrightness}
+			><Icon src={isDark ? Sun : Moon} width={18} height={18} class="inline mr-1" /></span
+		>
+		<select bind:value={theme} style="width: 16px;height: 18px;" id="themeSelector">
 			{#each themes as pTheme}
 				<option value={pTheme.value}>{pTheme.name}</option>
 			{/each}
 		</select>
-
-		<label for="toggle-dark" class="flex items-center cursor-pointer relative mb-4">
-			<span class="mr-3 text-gray-900 text-sm font-medium flex-grow">Dark Mode</span>
-			<input type="checkbox" id="toggle-dark" class="sr-only" bind:checked={isDark} />
-			<div
-				class="toggle-bg bg-gray-200 border-2 border-gray-200 h-6 mr-5 w-11 rounded-full self-end"
-			/>
-		</label>
 	</div>
 </header>
 
 <style lang="postcss">
-	.toggle-bg:after {
-		content: '';
-		@apply absolute top-0.5 right-11 bg-white border border-gray-300 rounded-full h-5 w-5 transition shadow-sm;
-	}
-	input:checked + .toggle-bg:after {
-		transform: translateX(100%);
-		@apply border-white;
-	}
-
-	input:checked + .toggle-bg {
-		@apply bg-blue-600 border-blue-600;
-	}
 </style>
