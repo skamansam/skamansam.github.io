c0ff4be8500481b6c24b49b23dadb13c22914bb9
skamansam <skamansam@users.noreply.github.com>
2022-07-31 20:51:02 +0000

    deploy: 0dc81defe04e5c5e85e506caf8144f69b7e80a22

---
+++ _app/immutable/pages/posts/2008-05-24-gsoc-day-0-equals-hit-the-ground-running.md-1605de2b.js
@@ -0,0 +1,21 @@
+import{S as T,i as b,s as j,D as S,x as E,y as q,z,A as B,B as x,r as D,p as G,C as H,L as N,e as y,t as f,k,c as I,a as v,h as m,d as i,m as L,b as $,g as h,I as g,n as R}from"../../chunks/index-f4167289.js";import{P as F}from"../../chunks/post-f1d237ea.js";import"../../chunks/stores-306c3674.js";import"../../chunks/settings-69239d06.js";function J(d){let t,s,r,o,e,n,u,C,c,p,P;return{c(){t=y("p"),s=f(`It is now day 0 for gSoC - the first day. I have hit the ground running and am off to a good
+start. I have already created the CPANPLUS::Shell integration and the file layout necessary
+for uploading to CPAN.`),r=k(),o=y("p"),e=f("Today, I plan on running some tests to gather output from CPANPLUS and posting notes on "),n=y("a"),u=f(`my
+project\u2019s wiki`),C=f(`. I plan on testing searching, listing
+installed modules, listing updated modules and parsing all this output into some form I
+can really use.`),c=k(),p=y("p"),P=f(`Come back later for more, as I plan on keeping updating this blog at every landmark
+accomplishment of this project.`),this.h()},l(a){t=I(a,"P",{});var l=v(t);s=m(l,`It is now day 0 for gSoC - the first day. I have hit the ground running and am off to a good
+start. I have already created the CPANPLUS::Shell integration and the file layout necessary
+for uploading to CPAN.`),l.forEach(i),r=L(a),o=I(a,"P",{});var _=v(o);e=m(_,"Today, I plan on running some tests to gather output from CPANPLUS and posting notes on "),n=I(_,"A",{href:!0,rel:!0});var A=v(n);u=m(A,`my
+project\u2019s wiki`),A.forEach(i),C=m(_,`. I plan on testing searching, listing
+installed modules, listing updated modules and parsing all this output into some form I
+can really use.`),_.forEach(i),c=L(a),p=I(a,"P",{});var w=v(p);P=m(w,`Come back later for more, as I plan on keeping updating this blog at every landmark
+accomplishment of this project.`),w.forEach(i),this.h()},h(){$(n,"href","http://wxcpanplus.googlecode.com"),$(n,"rel","nofollow")},m(a,l){h(a,t,l),g(t,s),h(a,r,l),h(a,o,l),g(o,e),g(o,n),g(n,u),g(o,C),h(a,c,l),h(a,p,l),g(p,P)},p:R,d(a){a&&i(t),a&&i(r),a&&i(o),a&&i(c),a&&i(p)}}}function K(d){let t,s;const r=[d[0],U];let o={$$slots:{default:[J]},$$scope:{ctx:d}};for(let e=0;e<r.length;e+=1)o=S(o,r[e]);return t=new F({props:o}),{c(){E(t.$$.fragment)},l(e){q(t.$$.fragment,e)},m(e,n){z(t,e,n),s=!0},p(e,[n]){const u=n&1?B(r,[n&1&&x(e[0]),n&0&&x(U)]):{};n&2&&(u.$$scope={dirty:n,ctx:e}),t.$set(u)},i(e){s||(D(t.$$.fragment,e),s=!0)},o(e){G(t.$$.fragment,e),s=!1},d(e){H(t,e)}}}const U={comments:!0,view:"post",layout:"post",lang:"en",author:"skamansam",title:"gSoC_Day[0]='Hit The Ground Running';",description:"It is now day 0 for gSoC",excerpt:"I have hit the ground running and am off to a good start",cover:!1,coverImage:"/images/GSoC-logo-horizontal-800.png",coverAlt:"RBE Logo",categories:["perl"],tags:["gSoC"],created_at:"2008-05-24 13:41",updated_at:"2008-05-24 13:41",readingTime:{text:"1 min read",minutes:.5,time:3e4,words:100},preview:`It is now day 0 for gSoC - the first day. I have hit the ground running and am off to a good 
+start. I have already created the CPANPLUS::Shell integration and the file layout necessary 
+for uploading to CPAN.
+
+Today, I plan on running some tests to...`,previewHtml:"<p>It is now day 0 for gSoC - the first day. I have hit the ground running and am off to a good start. I have already created the CPANPLUS::Shell integration and the file layout necessary for uploading to CPAN.</p> <p>Today, I plan on running some tests to gat...</p>",textContent:`It is now day 0 for gSoC - the first day. I have hit the ground running and am off to a good 
+start. I have already created the CPANPLUS::Shell integration and the file layout necessary 
+for uploading to CPAN.
+
+Today, I plan on running some tests to gather output from CPANPLUS and posting notes on...`};function M(d,t,s){return d.$$set=r=>{s(0,t=S(S({},t),N(r)))},t=N(t),[t]}class X extends T{constructor(t){super(),b(this,t,M,K,j,{})}}export{X as default,U as metadata};
