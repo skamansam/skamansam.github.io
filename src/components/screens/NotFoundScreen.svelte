b118b92fda4138336a34b901aeade0db28a14672
Samuel C. Tyler <skamansam@gmail.com>
2023-02-25 18:19:51 -0500

    add not found when applicable

---
+++ src/components/screens/NotFoundScreen.svelte
@@ -0,0 +1,5 @@
+<h2 class="italic">Psst!</h2>
+
+<h4>I don't know what you're looking for, but it's not here. Sorry.</h4>
+
+<p>Try one of the links above or the search feature. Maybe one of them can help you.</p>
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -10,17 +10,18 @@
 
 <script lang="ts">
   // import ErrorScreen from '../components/screens/ErrorScreen.svelte'; // your own Error screen component
-  // import NotFoundScreen from '../components/screens/NotFoundScreen.svelte'; // your own 404 screen component
+  import NotFoundScreen from '../components/screens/NotFoundScreen.svelte'; // your own 404 screen component
+  import { page } from '$app/stores';
 
-  export let message:string;
-  export let status:number;
+  export let message:string = $page.error?.message;
+  export let status:number = $page.status;
 </script>
-<svelte:head><title>{status}</title></svelte:head>
+<svelte:head><title>OOps, {status}</title></svelte:head>
 
-<h1>{status}</h1>
-<p>{message}</p>
 {#if status == 404} <!-- Used '==' instead of '===' to match string/number status code (just to be sure) -->
-  <!-- <NotFoundScreen /> -->
+  <NotFoundScreen />
 {:else}
+  <h1>{status}</h1>
+  <p>{message}</p>
   <!-- <ErrorScreen {message} {status} /> -->
 {/if}
