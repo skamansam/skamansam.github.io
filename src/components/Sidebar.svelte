885d1c7769c51d71908b8acee396271acb3a3196
Samuel C. Tyler <skamansam@gmail.com>
2023-02-25 18:21:50 -0500

    adjust sidebar so it works on mobile

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -2,7 +2,7 @@
 	import { page } from '$app/stores';
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import { Rss } from '@steeze-ui/heroicons';
-
+  import { browser } from '$app/environment';
 	let clazz = '';
 	export { clazz as class };
 
@@ -16,21 +16,70 @@
 		{ icon: Rss, link: '/rss.xml' }
 	];
 
+  let navOpen = false;
+  const toggleNav = () => {
+    navOpen = !navOpen;
+  }
+
 	const isActive = ({ link }: NavLink) => {
 		return link === $page.url.pathname;
 	};
+
+  if (browser) {
+    let xDown = null;
+    let yDown = null;
+
+    function handleTouchStart(evt) {
+        const firstTouch = evt.touches[0];
+        xDown = firstTouch.clientX;
+        yDown = firstTouch.clientY;
+    };
+
+    function handleTouchMove(evt) {
+        if ( ! xDown || ! yDown ) {
+            return;
+        }
+
+        const xUp = evt.touches[0].clientX;
+        const yUp = evt.touches[0].clientY;
+
+        const xDiff = xDown - xUp;
+        const yDiff = yDown - yUp;
+
+        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+            if ( xDiff > 10 ) {
+                /* right swipe */
+                navOpen = false;
+            } else {
+                /* left swipe */
+                navOpen = true;
+            }
+        } else {
+            if ( yDiff > 0 ) {
+                /* down swipe */
+            } else {
+                /* up swipe */
+            }
+        }
+        /* reset values */
+        xDown = null;
+        yDown = null;
+    };
+
+    document.addEventListener('touchstart', handleTouchStart, false);
+    document.addEventListener('touchmove', handleTouchMove, false);
+  }
+
 </script>
 
-<nav class="sidebar {clazz}">
-	<div class="menuButton">
-		<div class="menuButtonBar TopBar" />
-		<div class="menuButtonBar MiddleBar" />
-		<div class="menuButtonBar BottomBar" />
+<nav class="sidebar {clazz}" class:nav-open={navOpen}>
+	<div class="menuButton" on:click={toggleNav}>
+		<div class="hamburger"/>
 	</div>
 	<ul class="navMenu">
 		{#each nav as navItem}
 			<li class:active={isActive(navItem)}>
-				<a href={navItem.link}>
+				<a href={navItem.link} on:click={toggleNav}>
 					{#if navItem.icon}
 						<Icon src={Rss} theme="solid" width="20" height="20" class="color-gray-900" />
 					{/if}
@@ -42,44 +91,4 @@
 </nav>
 
 <style>
-	@media only screen and (max-width: 700px) {
-		.navMenu {
-      @apply hidden;
-		}
-		.menuButton {
-      @apply inline-block !important;
-		}
-	}
-	.menuButton {
-    @apply p-1 rounded-lg border-2 border-slate-600 hidden cursor-pointer;
-	}
-
-	.menuButtonBar {
-		width: 35px;
-		height: 5px;
-		background-color: #333;
-		margin: 6px 0;
-		transition: 0.4s;
-	}
-	.menuButton .menuButtonBar.MiddleBar,
-	.menuButton .menuButtonBar.BottomBar {
-		width: 70%;
-		margin-left: 30%;
-	}
-	/* Rotate first bar */
-	.menuButton:hover .menuButtonBar.TopBar {
-		-webkit-transform: rotate(-45deg) translate(-9px, 6px);
-		transform: rotate(-45deg) translate(-9px, 6px);
-	}
-
-	/* Fade out the second bar */
-	.menuButton:hover .menuButtonBar.MiddleBar {
-		opacity: 0;
-	}
-
-	/* Rotate last bar */
-	.menuButton:hover .menuButtonBar.BottomBar {
-		-webkit-transform: rotate(45deg) translate(-8px, -8px);
-		transform: rotate(45deg) translate(-8px, -8px);
-	}
 </style>
