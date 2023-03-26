fba282df8d5507d6b720944b3a06666e7a537e70
Samuel C. Tyler <skamansam@gmail.com>
2023-03-25 23:09:49 -0400

    change site name to RudeBoy.dev

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -23,7 +23,7 @@
 		<!-- <img src="/rbe-logo.svg" alt="Rude Boy Enterprises Logo" width="100" height="100"> -->
 	</div>
 	<div class="flex flex-col grow justify-center ml-3">
-		<div class="text-4xl text-onPrimaryBg"><a href="/" class="text-inherit no-underline">Rude Boy Solutions</a></div>
+		<div class="text-4xl text-onPrimaryBg"><a href="/" class="text-inherit no-underline">RudeBoy.dev</a></div>
 		<div class=" indent-5">Solutions. For Life.</div>
 	</div>
 	<div class="mt-2 mr-2 text-sm">
