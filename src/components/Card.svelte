07d345d66440eb124af857eda8051c61864b8f8e
Samuel C. Tyler <skamansam@gmail.com>
2023-03-27 09:49:11 -0400

    update cards to look better and more professional

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -13,8 +13,18 @@
 		/** @type string|null */
 		to = null,
     /** @type string|null */
-    style="";
+    style="",
+    /**@type string[] */
+    tags = []
+    ;
 </script>
+
+<style>
+  a.tag {
+    @apply text-tertiary bg-quaternary rounded-element mx-2 px-2 py-1 inline-block;
+    text-decoration: none;
+  }
+</style>
 
 <div class="card {clazz} text-onSecondaryBg bg-secondaryBg rounded-element p-4 my-4 drop-shadow-element" {style}>
   <section>
@@ -26,7 +36,7 @@
           {/each}
         </slot>
       </div>
-      <div class="card-title text-xl font-bold">
+      <div class="card-title text-2xl font-bold">
         {#if to}
           <a href={to} class="decoration-transparent">{title}</a>
         {:else}
@@ -34,7 +44,7 @@
         {/if}
       </div>
       {#if subtitle}
-      <div class="card-subtitle text-xs">
+      <div class="card-subtitle text-md">
         <slot name="subtitle">{subtitle}</slot>
       </div>
       {/if}
@@ -47,5 +57,21 @@
     <div class="card-body">
       <slot />
     </div>
+        <!-- <span slot="badges">
+      {#each (post.meta?.categories || []) as category}
+        <Badge text={category} to="/categories/{category}"/>
+      {/each}
+    </span> -->
+    <!-- <span slot="metadata">
+      Published {new Date(post.meta.created_at).toLocaleString()}
+    </span> -->
+    <div class="tags my-3">
+      <slot name="tags">
+      {#each (tags || []) as tag}
+        <a href="/tags/{tag}" class="tag">#{tag}</a>
+      {/each}
+      </slot>
+    </div>
+
   </section>
 </div>
