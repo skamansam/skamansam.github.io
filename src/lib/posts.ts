1302fc481ac52204d907b6a6363b9094b898f163
Samuel C. Tyler <skamansam@gmail.com>
2023-03-27 09:51:56 -0400

    add reading time to post api

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,10 +1,14 @@
+import type { PostJSON } from '$lib/types';
+import readingTime from 'reading-time';
 export const allPostFiles = import.meta.glob('../routes/posts/**/*.md');
 
 export const allPosts:PostJSON[] = await Promise.all(
   Object.entries(allPostFiles).map(async ([path, resolver]) => {
-    const { metadata } = await resolver()
+    const fileData = await resolver();
+    const { metadata } = fileData;
     const pathAry = path.split('/');
     const sitePath = pathAry.slice(-2).join('/').slice(0,-3);
+    metadata.readingTime = readingTime(metadata.textContent).text
 
     return {
       meta: metadata,
@@ -17,7 +21,7 @@ export const allPosts:PostJSON[] = await Promise.all(
 export const publishedPosts = allPosts.filter((post) => !(post.meta.draft))
 
 export const sortedPosts = publishedPosts.sort((a, b) => {
-  return new Date(a.meta.created_at) < new Date(b.meta.created_at) ? 1 : -1 
+  return new Date(a.meta.created_at) < new Date(b.meta.created_at) ? 1 : -1
 })
 
 export default {
