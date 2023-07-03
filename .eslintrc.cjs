1482a401177945bd0c1f59741e2b6d9104c9e2f3
Samuel C. Tyler <skamansam@gmail.com>
2023-07-03 16:38:25 -0400

    update dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,12 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:svelte/recommended'],
+	plugins: ['svelte', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte-eslint-parser' }],
 	settings: {
-		'svelte3/typescript': () => require('typescript')
+		'svelte/typescript': () => require('typescript')
 	},
 	parserOptions: {
 		sourceType: 'module',
