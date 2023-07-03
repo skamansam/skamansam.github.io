0b10837c57c903b3514eeaf9d30e39d88eb18250
Samuel C. Tyler <skamansam@gmail.com>
2023-07-03 11:31:17 -0400

    add a little bio, for copy/pasting into 3rd party sited

---
+++ src/routes/portfolio/bio/+page.svelte
@@ -0,0 +1,36 @@
+<p>
+	I started building web sites in 1996 and quickly moved to web applications in Perl::CGI on Linux
+	servers. I went on to build several desktop apps &nbsp;in Perl::Gtk, Perl::Tk, and
+	Perl::WxWidgets, mostly for systems management. I built my first blog in 1999, called MeHome (now
+	PSaMs) and have been working with dynamic web sites ever since. I was awarded a chance to work
+	with Google during the Summer of Code 2008, where I developed a frontend to Perl::CPANPLUS, called <a
+		href="https://code.google.com/archive/p/wxcpan/">WxCPAN</a
+	>.&nbsp;
+</p>
+<p>
+	After graduating college, I landed a job with MDLogix, working with Ruby on Rails with
+	&nbsp;custom frontends. As project lead there, I oversaw the design and development of many web
+	applications used throught the healthcare industry, including mobile surveys for psychological
+	diagnosis in children, the elderly, and soldiers on the battlefield. I also developed a social
+	network analysis and visualization application, Socioworks, used in crisis management. I also
+	helped develop the initial version of BHWorks, the company's current flagship product.&nbsp;
+</p>
+<p>
+	After working in the healthcare industry for nearly 5 years, I joined the private sector and
+	worked with the reverse-logistics company, Optoro, initially working on the company-facing
+	software used by company managament and accounting. After a year, I moved to working solely on the
+	inventory management tools used on-site at our warehouse. As part of this team, I helped create
+	the user experiences, microservices, and logic to speed up inventory throughput. I was also on the
+	launch team for Optoro's current B2B soolution - BULQ.com.&nbsp;
+</p>
+<p>
+	My current position is full-stack developer for RedShred, LLC, where I have developed several
+	versions of the public API used to interact with our AIs that power the business. My first task
+	was to develop and deploy a kubernetes cluster, which I did from scratch (using the raw YAML
+	instead of management tools such as Helm). I have also developed a web application that uses the
+	API I developed to interact with our data, in a PDF-viewer style.&nbsp;
+</p>
+<p>
+	I have helped develop many applications in my years as a software developer and plan to help
+	create many more. Maybe even yours!
+</p>
